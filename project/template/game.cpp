c442df720b6e0d2ddca7ef5dad1183cd76be4f43
Jeremie Quesnel <quesnel.jeremie@gmail.com>
2015-12-20 11:30:37 +0100

    add skybox with few bugs,i'm gonna fix it

--- project/template/game.cpp
+++ project/template/game.cpp
@@ -11,9 +11,39 @@
 #include "include/camera.hpp"
 
 using namespace glimac;
+using namespace std;
 
 // Light attributes
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+GLuint loadCubemap(vector<const GLchar*> faces)
+{
+  GLuint textureID;
+  glGenTextures(1, &textureID);
+  glActiveTexture(GL_TEXTURE0);
+
+  int width,height;
+  std::unique_ptr<Image> image;
+
+  glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
+  for(GLuint i = 0; i < faces.size(); i++)
+  {
+      //image = SOIL_load_image(faces[i], &width, &height, 0, SOIL_LOAD_RGB);
+      image = loadImage(faces[i]);
+      //width = image->getWidth();
+      //height = image->getHeight();
+      glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i,0,GL_RGBA,image->getWidth(),image->getHeight(),0,GL_RGBA,GL_FLOAT,image->getPixels());
+      //glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+  }
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+  glBindTexture(GL_TEXTURE_CUBE_MAP, 0);
+
+  return textureID;
+}
 
 int main(int argc, char** argv) {
     GLuint screenWidth = 800, screenHeight = 600;
@@ -24,7 +54,7 @@ int main(int argc, char** argv) {
     glewExperimental = GL_TRUE;
     GLenum glewInitError = glewInit();
     if(GLEW_OK != glewInitError) {
-        std::cerr << glewGetErrorString(glewInitError) << std::endl;
+        cerr << glewGetErrorString(glewInitError) << endl;
         return EXIT_FAILURE;
     }
 
@@ -34,19 +64,88 @@ int main(int argc, char** argv) {
     // Setup some OpenGL options
     glEnable(GL_DEPTH_TEST);
 
-    std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
-    std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl;
+    cout << "OpenGL Version : " << glGetString(GL_VERSION) << endl;
+    cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << endl;
 
 
     // Setup and compile our shaders
     Shader MyShader("template/shaders/model_loading.vs.glsl", "template/shaders/model_loading.fs.glsl");
     Shader AmbientLighting("template/shaders/ambiant_lighting.vs.glsl", "template/shaders/ambiant_lighting.fs.glsl");
     Shader PointLighting("template/shaders/point_lighting.vs.glsl", "template/shaders/point_lighting.fs.glsl");
+    Shader skyboxShader("template/shaders/skybox.vs.glsl", "template/shaders/skybox.fs.glsl");
 
     // Load models
     Model crysis("assets/models/nanosuit/nanosuit.obj");
     Model house("assets/models/house/fw43_lowpoly_n1.3ds");
     Model landscape("assets/models/castle/eastern ancient casttle/eastern ancient casttle.obj");
+
+    GLfloat skyboxVertices[] = {
+            // Positions
+            -50.0f,  50.0f, -50.0f,
+            -50.0f, -50.0f, -50.0f,
+             50.0f, -50.0f, -50.0f,
+             50.0f, -50.0f, -50.0f,
+             50.0f,  50.0f, -50.0f,
+            -50.0f,  50.0f, -50.0f,
+
+            -50.0f, -50.0f,  50.0f,
+            -50.0f, -50.0f, -50.0f,
+            -50.0f,  50.0f, -50.0f,
+            -50.0f,  50.0f, -50.0f,
+            -50.0f,  50.0f,  50.0f,
+            -50.0f, -50.0f,  50.0f,
+
+             50.0f, -50.0f, -50.0f,
+             50.0f, -50.0f,  50.0f,
+             50.0f,  50.0f,  50.0f,
+             50.0f,  50.0f,  50.0f,
+             50.0f,  50.0f, -50.0f,
+             50.0f, -50.0f, -50.0f,
+
+            -50.0f, -50.0f,  50.0f,
+            -50.0f,  50.0f,  50.0f,
+             50.0f,  50.0f,  50.0f,
+             50.0f,  50.0f,  50.0f,
+             50.0f, -50.0f,  50.0f,
+            -50.0f, -50.0f,  50.0f,
+
+            -50.0f,  50.0f, -50.0f,
+             50.0f,  50.0f, -50.0f,
+             50.0f,  50.0f,  50.0f,
+             50.0f,  50.0f,  50.0f,
+            -50.0f,  50.0f,  50.0f,
+            -50.0f,  50.0f, -50.0f,
+
+            -50.0f, -50.0f, -50.0f,
+            -50.0f, -50.0f,  50.0f,
+             50.0f, -50.0f, -50.0f,
+             50.0f, -50.0f, -50.0f,
+            -50.0f, -50.0f,  50.0f,
+             50.0f, -50.0f,  50.0f
+        };
+
+        // Setup skybox VAO
+        GLuint skyboxVAO, skyboxVBO;
+        glGenVertexArrays(1, &skyboxVAO);
+        glGenBuffers(1, &skyboxVBO);
+        glBindVertexArray(skyboxVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        glBindVertexArray(0);
+
+        // Cubemap (Skybox)
+        vector<const GLchar*> faces;
+        faces.push_back("assets/textures/right.jpg");
+        faces.push_back("assets/textures/left.jpg");
+        faces.push_back("assets/textures/top.jpg");
+        faces.push_back("assets/textures/bottom.jpg");
+        faces.push_back("assets/textures/back.jpg");
+        faces.push_back("assets/textures/front.jpg");
+        GLuint cubemapTexture = loadCubemap(faces);
+
+
 
     //Point light position
     glm::vec3 pointLightPositions[] {
@@ -98,7 +197,26 @@ int main(int argc, char** argv) {
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+        glm::mat4 view = camera.getViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
+
+        // Draw skybox first
+        glDepthMask(GL_FALSE);// Remember to turn depth writing off
+        skyboxShader.Use();
+
+        glUniformMatrix4fv(glGetUniformLocation(skyboxShader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(glGetUniformLocation(skyboxShader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+        // skybox cube
+        glBindVertexArray(skyboxVAO);
+        glActiveTexture(GL_TEXTURE0);
+        glUniform1i(glGetUniformLocation(skyboxShader.Program, "skybox"), 0);
+        glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+        glDepthMask(GL_TRUE);
+
         /* POINTLIGHT */
+        /*
         PointLighting.Use();
         // Point light 1
         glUniform3f(glGetUniformLocation(PointLighting.Program, "pointLights[0].position"), pointLightPositions[0].x, pointLightPositions[0].y, pointLightPositions[0].z);
@@ -116,27 +234,26 @@ int main(int argc, char** argv) {
         glUniform1f(glGetUniformLocation(PointLighting.Program, "pointLights[1].constant"), 0.40f);
         glUniform1f(glGetUniformLocation(PointLighting.Program, "pointLights[1].linear"), 0.09);
         glUniform1f(glGetUniformLocation(PointLighting.Program, "pointLights[1].quadratic"), 0.032);
+        */
 
+        // Use cooresponding shader when setting uniforms/drawing objects
+        AmbientLighting.Use();
+        GLint lightPosLoc = glGetUniformLocation(AmbientLighting.Program, "light.position");
+        GLint viewPosLoc = glGetUniformLocation(AmbientLighting.Program, "viewPos");
+        glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
+        glUniform3f(viewPosLoc, lightPos.x, lightPos.y, lightPos.z);
+        // Set lights properties
+        glUniform3f(glGetUniformLocation(AmbientLighting.Program, "light.ambient"),  0.2f, 0.2f, 0.2f);
+        glUniform3f(glGetUniformLocation(AmbientLighting.Program, "light.diffuse"),  0.5f, 0.5f, 0.5f);
+        glUniform3f(glGetUniformLocation(AmbientLighting.Program, "light.specular"), 1.0f, 1.0f, 1.0f);
+        // Set material properties
+        glUniform1f(glGetUniformLocation(AmbientLighting.Program, "material.shininess"), 32.0f);
 
-
-                // Use cooresponding shader when setting uniforms/drawing objects
-                AmbientLighting.Use();
-                GLint lightPosLoc    = glGetUniformLocation(AmbientLighting.Program, "light.position");
-                GLint viewPosLoc     = glGetUniformLocation(AmbientLighting.Program, "viewPos");
-                glUniform3f(lightPosLoc, lightPos.x, lightPos.y, lightPos.z);
-                glUniform3f(viewPosLoc, lightPos.x, lightPos.y, lightPos.z);
-                // Set lights properties
-                glUniform3f(glGetUniformLocation(AmbientLighting.Program, "light.ambient"),  0.2f, 0.2f, 0.2f);
-                glUniform3f(glGetUniformLocation(AmbientLighting.Program, "light.diffuse"),  0.5f, 0.5f, 0.5f);
-                glUniform3f(glGetUniformLocation(AmbientLighting.Program, "light.specular"), 1.0f, 1.0f, 1.0f);
-                // Set material properties
-                glUniform1f(glGetUniformLocation(AmbientLighting.Program, "material.shininess"), 32.0f);
-
-
-
+/*
         // Transformation matrices
         glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
         glm::mat4 view = camera.getViewMatrix();
+*/
 
         /* MYSHADER
         MyShader.Use();
